f107ce06187c54fea1728132d33056e362b2a794
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-11-08 12:49:21 +0900

    いっぬ

--- main.go
+++ main.go
@@ -12,15 +12,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/mattn/go-sixel"
 	"github.com/nfnt/resize"
 )
 
-func main() {
+func nyanko() (string, error) {
 	resp, err := http.Get("https://aws.random.cat/meow")
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	defer resp.Body.Close()
 	var r struct {
@@ -28,10 +29,44 @@ func main() {
 	}
 	err = json.NewDecoder(resp.Body).Decode(&r)
 	if err != nil {
+		return "", err
+	}
+	return r.File, nil
+}
+
+func innu() (string, error) {
+	resp, err := http.Get("https://random.dog/woof.json")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	var r struct {
+		URL string `json:"url"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		return "", err
+	}
+	return r.URL, nil
+}
+
+func main() {
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var uri string
+	switch filepath.Base(exe) {
+	case "nyanko":
+		uri, err = nyanko()
+	case "innu":
+		uri, err = innu()
+	}
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	resp, err = http.Get(r.File)
+	resp, err := http.Get(uri)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,5 +85,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(r.File)
+	fmt.Println(uri)
 }
